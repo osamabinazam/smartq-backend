7052f8451aa7f7918a1a98bf93477a5a9500230f
Osama Bin Azam <osamabinazam@gmail.com>
2024-03-25 14:42:15 +0500

    Add authenticat middleware

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -1,10 +1,13 @@
-import AuthController from "../controllers/AuthController";
+import AuthController from "../controllers/AuthController.js";
+import authenticateToken from "../middlewares/authorization.js";
 import express from 'express';
 const router = express.Router();
 
-router.post('/login', AuthController.login);
-router.post('/reset-password', AuthController.resetPassword);
+
+router.post('/login' ,AuthController.login);
+router.post('/reset-password', authenticateToken, AuthController.resetPassword);
 router.post('/register', AuthController.register);
-router.post('/logout', AuthController.logout);
+router.post('/logout',authenticateToken ,AuthController.logout);
+
 
 export default router;
