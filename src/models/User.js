b16da2ac1aebe9b0a20ede140195d45590d9770d
Osama Bin Azam <osamabinazam@gmail.com>
2024-03-25 14:44:28 +0500

    Update user Model

--- src/models/User.js
+++ src/models/User.js
@@ -20,105 +20,19 @@ const UserModel = (sequelize) => {
    * @param {Object} fields - The fields to define the User model.
    */
   User.init({
-    /**
-     * Unique identifier for each user.
-     * Automatically generated as UUIDv4.
-     * @type {DataTypes.UUID}
-     */
-    userid: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true
-    },
-    /**
-     * Username of the user.
-     * Must be unique and not null.
-     * @type {DataTypes.STRING}
-     */
-    username: {
-      type: DataTypes.STRING(50),
-      allowNull: false,
-      unique: true
-    },
-    /**
-     * Email of the user.
-     * Must be unique and not null.
-     * @type {DataTypes.STRING}
-     */
-    email: {
-      type: DataTypes.STRING(100),
-      allowNull: false,
-      unique: true
-    },
-    /**
-     * Gender of the user.
-     * Can be 'male', 'female', or 'other'.
-     * @type {DataTypes.ENUM}
-     */
-    gender: {
-      type: DataTypes.ENUM('male', 'female', 'other')
-    },
-    /**
-     * Password of the user.
-     * Not null.
-     * @type {DataTypes.STRING}
-     */
-    password: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
-    /**
-     * Date of user registration.
-     * Defaults to current date and time.
-     * @type {DataTypes.DATE}
-     */
-    registrationdate: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW
-    },
-    /**
-     * Date of the last login.
-     * Initially null.
-     * @type {DataTypes.DATE}
-     */
-    lastlogindate: {
-      type: DataTypes.DATE,
-      defaultValue: null
-    },
-    /**
-     * Type of user.
-     * Can be 'vendor', 'customer', or 'admin'.
-     * Defaults to 'customer'.
-     * @type {DataTypes.ENUM}
-     */
-    usertype: {
-      type: DataTypes.ENUM('vendor', 'customer', 'admin'),
-      defaultValue: 'customer'
-    },
-    /**
-     * Indicates whether the user is active or not.
-     * Defaults to true.
-     * @type {DataTypes.BOOLEAN}
-     */
-    isactive: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true
-    }
+    
+    userid: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
+    username: { type: DataTypes.STRING(50), allowNull: false, unique: true },
+    email: { type: DataTypes.STRING(100),allowNull: false,unique: true },
+    gender: {type: DataTypes.ENUM('male', 'female', 'other') },
+    password: { type: DataTypes.STRING(255), allowNull: false },
+    lastlogin: { type: DataTypes.DATE, defaultValue: null },
+    usertype: { type: DataTypes.ENUM('vendor', 'customer', 'admin'), defaultValue: 'customer' },
+    isactive: { type: DataTypes.BOOLEAN, defaultValue: true }
   }, {
-    /**
-     * Sequelize instance for connecting to the database.
-     * @type {*}
-     */
+    
     sequelize,
-    /**
-     * Model name for referencing.
-     * @type {string}
-     */
     modelName: 'User',
-    /**
-     * Table name in the database.
-     * @type {string}
-     */
     tableName: 'users'
   });
 
