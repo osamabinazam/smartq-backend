d0d82e526b9d1a0f3584cae9a6e6038c8615b888
Osama Bin Azam <osamabinazam@gmail.com>
2024-05-01 00:09:14 +0500

    Update associations

--- src/models/associations/associations.js
+++ src/models/associations/associations.js
@@ -40,9 +40,9 @@ const setupAssociations = (db) => {
     db.VendorProfileModel.hasMany(db.RequestModel, { foreignKey: 'vendorprofileid', as: 'requests' });
     db.RequestModel.belongsTo(db.VendorProfileModel, { foreignKey: 'vendorprofileid', as: 'vendor_profile' });
 
-    // VendorProfile and Category (Note Vendor can have multiple categories)
-    db.VendorProfileModel.hasMany(db.CategoryModel, { foreignKey: 'vendorprofileid', as: 'categories' });
-    db.CategoryModel.belongsTo(db.VendorProfileModel, { foreignKey: 'vendorprofileid', as: 'vendor_profile' });
+    // // VendorProfile and Category (Note Vendor can have multiple categories)
+    // db.VendorProfileModel.hasMany(db.CategoryModel, { foreignKey: 'vendorprofileid', as: 'categories' });
+    // db.CategoryModel.belongsTo(db.VendorProfileModel, { foreignKey: 'vendorprofileid', as: 'vendor_profile' });
 
     // VendorProfile and Location (Note Vendor can have multiple locations at different times or days)
     db.VendorProfileModel.hasMany(db.LocationModel, { foreignKey: 'vendorprofileid', as: 'business_locations' });
@@ -56,12 +56,26 @@ const setupAssociations = (db) => {
     db.VendorProfileModel.hasMany(db.QueueModel, { foreignKey: 'vendorprofileid', as: 'queues' });
     db.QueueModel.belongsTo(db.VendorProfileModel, { foreignKey: 'vendorprofileid', as: 'vendor_profile' });
 
-    // VendorProfile and Service (Note Vendor can provide multiple services)
-    db.VendorProfileModel.hasMany(db.ServiceModel, { foreignKey: 'vendorprofileid', as: 'services' });
-    db.ServiceModel.belongsTo(db.VendorProfileModel, { foreignKey: 'vendorprofileid', as: 'vendor_profile' });
+    // VendorProfile and Service (Note Vendor can have multiple services and service can be provided by multiple vendors)
+   // Define the many-to-many relationship between VendorProfileModel and ServiceModel
+db.VendorProfileModel.belongsToMany(db.ServiceModel, {
+    through: 'VendorService', // Name of the join table
+    foreignKey: 'vendorprofileid', // Foreign key in the join table for VendorProfileModel
+    otherKey: 'serviceid', // Foreign key in the join table for ServiceModel
+    as: 'services', // Alias for the association
+    // through: { model: db.VendorService, unique: false, timestamps: false } 
 
+  });
+  
+  db.ServiceModel.belongsToMany(db.VendorProfileModel, {
+    through: 'VendorService', // Name of the join table
+    foreignKey: 'serviceid', // Foreign key in the join table for ServiceModel
+    otherKey: 'vendorprofileid', // Foreign key in the join table for VendorProfileModel
+    as: 'vendors', // Alias for the association
+    // through: { model: db.VendorService, unique: false, timestamps: false }
+  });
     // VendorProfile and SocialMedia (Note Vendor can have multiple social media accounts for their business profile)
-    db.VendorProfileModel.hasMany(db.SocialMediaModel, { foreignKey: 'vendorprofileid', as: 'socialMedia' });
+    db.VendorProfileModel.hasMany(db.SocialMediaModel, { foreignKey: 'vendorprofileid', as: 'social_media' });
     db.SocialMediaModel.belongsTo(db.VendorProfileModel, { foreignKey: 'vendorprofileid', as: 'vendor_profile' });
 
     // VendorProfile and Education (Note Vendor can have multiple educational qualifications)
@@ -129,6 +143,11 @@ const setupAssociations = (db) => {
     // User and Image (Note User can have multiple images)
     db.UserModel.hasMany(db.ImageModel, { foreignKey: 'userid', as: 'images' });
     db.ImageModel.belongsTo(db.UserModel, { foreignKey: 'userid', as: 'user' });
+
+   
+   // Queue and Servide (Note: Service can have multiple queues)
+    db.ServiceModel.hasMany(db.QueueModel, { foreignKey: 'serviceid', as: 'queues' });
+    db.QueueModel.belongsTo(db.ServiceModel, { foreignKey: 'serviceid' , as: 'services' });
 
 };
 
--- src/models/index.js
+++ src/models/index.js
@@ -13,41 +13,38 @@ const sequelize = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.passw
 
 /**
  * Initialize the database with models and associations
- * 
  */
-async function initializeDatabase() {
+// async function initializeDatabase() {
   try {
-    await sequelize.authenticate();
+     sequelize.authenticate();
     console.log('Connection has been established successfully.');
 
-    const modelFiles = fs.readdirSync(__dirname)                              // Read all files in the current directory
-      .filter(file => file.indexOf('.') !== 0 && file.slice(-3) === '.js');   // Get all model files
+    const modelFiles = fs.readdirSync(__dirname)
+      .filter(file => file.indexOf('.') !== 0 && file.slice(-3) === '.js'); // Get all model files
 
     for (const file of modelFiles) {
       const modelInit = require(path.join(__dirname, file)); // Import the model file
       
-      // Check if the modelInit is a function
       if (typeof modelInit === 'function') {
         const model = modelInit(sequelize); // Invoke the function with the Sequelize instance
         db[model.name + "Model"] = model;
       }
     }
-    setupAssociations(db);      // Setup model associations
-    db.sequelize = sequelize;   // Export the Sequelize instance
-    db.Sequelize = Sequelize;   // Export Sequelize
-
-    await sequelize.sync({ force: false, alter: true }); // Sync all models with the database (force: true will drop the tables)
+    setupAssociations(db); // Setup model associations
+     sequelize.sync({ force: false, alter: true }); // Sync all models with the database
     console.log('All models were synchronized successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
+    return; // Exit if there's a database connection error
   }
-}
 
+// }
 
-initializeDatabase();         // Initialize the database
+// initializeDatabase();
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
 /**
  * Export the db object
- * 
  */
 module.exports = db;
