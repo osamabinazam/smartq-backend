d63f87bcb4618b2e7849fd49b8511c4555c2e44a
Osama Bin Azam <osamabinazam@gmail.com>
2024-05-05 20:39:39 +0500

    Reset password routes

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -3,9 +3,12 @@ const authenticateToken = require("../middlewares/authorization.js");
 const express = require('express');
 const router = express.Router();
 
+
+router.post('/new-password', AuthController.resetPassword);
 router.post('/login', AuthController.login);
-router.post('/reset-password', authenticateToken, AuthController.resetPassword);
-router.post('/forgot-password', AuthController.forgotPassword);
+router.post('/reset-password', AuthController.forgotPassword);
+
+router.post('/verify-otp', AuthController.verifyOTP);
 router.post('/register', AuthController.register);
 router.post('/logout', authenticateToken, AuthController.logout);
 
